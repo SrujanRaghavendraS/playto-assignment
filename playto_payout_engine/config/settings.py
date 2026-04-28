7fed627afc4fe083d60d6ca34f1885dd3960c9a0
Srujan Raghavendra <srujan9712@gmail.com>
2026-04-28 11:34:10 +0530

    changes

--- playto_payout_engine/config/settings.py
+++ playto_payout_engine/config/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -160,10 +162,14 @@ LOGGING = {
             "level": "INFO",
             "propagate": True,
         },
-        "core": {   # 👈 your app name (important)
+        "core": {
             "handlers": ["console"],
             "level": "INFO",
             "propagate": False,
         },
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
